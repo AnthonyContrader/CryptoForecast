1fcadfe85b60172b9d9178243455e5fafeb43aa2
diego271 <55692827+diego271@users.noreply.github.com>
2019-09-30 16:30:28 +0200

    updatecoin

---
+++ src/main/java/it/contrader/model/Coin.java
@@ -0,0 +1,119 @@
+package it.contrader.model;
+/**
+ * Per dettagli vedi guida sez 4 Model
+ */
+public class Coin {
+
+	/**
+	 * Qui sotto definisco gli attributi di Coin. 
+	 */
+	private int idcoin;
+
+	private String name;
+	
+	private int quotation;
+	
+	private String symbol;
+
+	
+	// costruttore vuoto
+	public Coin() {
+		
+	}
+	
+	// costruttore senza id
+
+	public Coin(String name, int quotation, String symbol) {
+		
+		this.name = name;
+		this.quotation = quotation;
+		this.symbol = symbol;
+	}
+    
+	// costruttore completo
+	
+	public Coin(int idcoin, String name, int quotation, String symbol) {
+		
+    	this.idcoin = idcoin;
+		this.name = name;
+		this.quotation = quotation;
+		this.symbol = symbol;
+	}
+
+	// gettere e setter
+	
+	
+	public int getId() {
+		return idcoin;
+	}
+
+
+	public void setId(int idcoin) {
+		this.idcoin = idcoin;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getQuotation() {
+		return quotation;
+	}
+
+
+	public void setQuotation(int quotation) {
+		this.quotation = quotation;
+	}
+
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	@Override
+	public String toString() {
+		return  idcoin + "\t"  + name +"\t\t" + quotation + "\t\t" + symbol;
+	}
+	
+	//Metodo per il confronto degli oggetti
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Coin other = (Coin) obj;
+			if (idcoin != other.idcoin)
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (quotation == 0) {
+				if (other.quotation != 0)
+					return false;
+			} else if (quotation != (other.quotation))
+				return false;
+			if (symbol == null) {
+				if (other.symbol != null)
+					return false;
+			} else if (!symbol.equals(other.symbol))
+				return false;
+			return true;
+		}
+	}
