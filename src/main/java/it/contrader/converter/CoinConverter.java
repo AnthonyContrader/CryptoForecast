6b5566fb443926c50da9912492db5164d8709a9e
giuseppe15889 <55691296+giuseppe15889@users.noreply.github.com>
2019-09-30 16:50:25 +0200

    upadtecoinview

---
+++ src/main/java/it/contrader/converter/CoinConverter.java
@@ -0,0 +1,59 @@
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.CoinDTO;
+import it.contrader.model.Coin;
+
+/**
+ * 
+ * @author team GDA
+ * 
+ *         Implementando questa l'interfaccia converter la classe CoinConverter
+ *         è OBBLIGATA ad implementarne i metodi
+ *
+ */
+public class CoinConverter  {
+
+	/**
+	 * Crea un oggetto di tipo CoinDTO e lo riempie con i campi del parametro coin
+	 * di tipo Coin. Notare l'uso del metodo get() per ottenere il valore
+	 * dell'attributo-
+	 */
+	
+	public CoinDTO toDTO(Coin coin) {
+		CoinDTO coinDTO = new CoinDTO(coin.getId(), coin.getName(), coin.getQuotation(), coin.getSymbol());
+		return coinDTO;
+		}
+	
+
+	/**
+	 * Crea un oggetto di tipo Coin e lo riempie con i campi del parametro coin di
+	 * tipo CoinDTO. Notare l'uso del metodo get() per ottenere il valore
+	 * dell'attributo-
+	 */
+	
+	public Coin toEntity(CoinDTO coinDTO) {
+		Coin coin = new Coin(coinDTO.getId(), coinDTO.getName(), coinDTO.getQuotation(), coinDTO.getSymbol());
+		return coin;
+	}
+
+	/**
+	 * Metodo per convertire le liste di Coin.
+	 */
+	
+	public List<CoinDTO> toDTOList(List<Coin> coinList) {
+		// Crea una lista vuota.
+		List<CoinDTO> coinDTOList = new ArrayList<CoinDTO>();
+
+		// Cicla tutti gli elementi della lista e li converte uno a uno
+		for (Coin coin : coinList) {
+			// Utilizza il metodo toDTO per convertire ogni singolo elemento della lista
+			// e lo aggiunge adda lista di DTO
+			coinDTOList.add(toDTO(coin));
+		}
+		return coinDTOList;
+	}
+
+}
---
+++ src/main/java/it/contrader/converter/TrendConverter.java
@@ -0,0 +1,65 @@
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.TrendDTO;
+import it.contrader.model.Trend;
+
+/**
+ * 
+ * @author GDA
+ * 
+ *         Implementando questa l'interfaccia converter la classe TrendConverter
+ *         è OBBLIGATA ad implementarne i metodi
+ *
+ */
+public class TrendConverter  {
+
+	/**
+	 * Crea un oggetto di tipo TrendDTO e lo riempie con i campi del parametro trend
+	 * di tipo Trend. Notare l'uso del metodo get() per ottenere il valore
+	 * dell'attributo-
+	 */
+	
+	public TrendDTO toDTO(Trend trend) {
+		TrendDTO trendDTO = new TrendDTO(trend.getId(), trend.getTime(), trend.getVariation(), trend.getNamecoin());
+		return trendDTO;
+		}
+	
+
+	/**
+	 * Crea un oggetto di tipo Trend e lo riempie con i campi del parametro trend di
+	 * tipo TrendDTO. Notare l'uso del metodo get() per ottenere il valore
+	 * dell'attributo-
+	 */
+	
+	public Trend toEntity(TrendDTO trendDTO) {
+		Trend trend = new Trend();
+		if (trendDTO != null) {
+			trend.setId(trendDTO.getId());
+			trend.setTime(trendDTO.getTime());
+			trend.setVariation(trendDTO.getVariation());
+			trend.setNamecoin(trendDTO.getNamecoin());
+		}
+		return trend;
+	}
+
+	/**
+	 * Metodo per convertire le liste di Trend.
+	 */
+	
+	public List<TrendDTO> toDTOList(List<Trend> trendList) {
+		// Crea una lista vuota.
+		List<TrendDTO> trendDTOList = new ArrayList<TrendDTO>();
+
+		// Cicla tutti gli elementi della lista e li converte uno a uno
+		for (Trend trend : trendList) {
+			// Utilizza il metodo toDTO per convertire ogni singolo elemento della lista
+			// e lo aggiunge adda lista di DTO
+			trendDTOList.add(toDTO(trend));
+		}
+		return trendDTOList;
+	}
+
+}
